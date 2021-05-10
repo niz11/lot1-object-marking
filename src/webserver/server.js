6904895721a6ec63f70127ab2135a18345868413
Niz <nizan.goldstein@gmail.com>
2021-05-10 11:37:20 +0300

    Adding needed SSL parts to express server

--- src/webserver/server.js
+++ src/webserver/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
+const https = require('https')
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const certOptions = {
+    key: fs.readFileSync(path.resolve('server.key')),
+    cert: fs.readFileSync(path.resolve('server.crt'))
+  }
+const server = https.createServer(certOptions, app)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 app.set('trust proxy', 'loopback');
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server is listening on ${PORT}`);
 });
