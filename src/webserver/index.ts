0b3bf99b5e0a77f98372d5240b037e5d41f4c360
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2021-05-14 13:57:49 +0200

    added static file handling

--- src/webserver/index.ts
+++ src/webserver/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import path from "path";
 
 
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-    console.log('received request!');
-    res.send('Express + TypeScript Server');
-});
+// app.get('/', (req, res) => {
+//     console.log('received request!');
+//     res.send('Express + TypeScript Server');
+// });
+app.use(express.static(path.join(__dirname,'public')));
 app.set('trust proxy', 'loopback');
 app.listen(port, 'localhost', () => {
     console.log(`server is listening on ${port}`);
