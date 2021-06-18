bda6d3b956e7d78d3fb0fee40b3854776a3751b4
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2021-06-18 15:49:26 +0200

    make use of mongoDB optional (for testing purposes)

--- src/webserver/index.js
+++ src/webserver/index.js
@@ -8,23 +8,24 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
-
+const useMongoDB = true;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
-
-mongoose.connect(
-    process.env.MONGO_URI,
-    {
-        useUnifiedTopology: true
-    },
-    (err, client) => {
-        if (err) return console.error(err);
-        console.log('Connected to Database');
-    }
-);
+if (useMongoDB) {
+    mongoose.connect(
+        process.env.MONGO_URI,
+        {
+            useUnifiedTopology: true
+        },
+        (err, client) => {
+            if (err) return console.error(err);
+            console.log('Connected to Database');
+        }
+    );
+}
 
 app.use(express.static(path.join(__dirname,'static')));
 app.use('/models', models);
