6fde7abce31a2b85c67967c42437ff7e98e0c7f4
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2021-06-28 01:25:55 +0200

    implemented opencv 4.5.2 aruco marker detection

--- src/webserver/static/offscreenworker.js
+++
@@ -1,7 +0,0 @@
-onmessage = function(evt) {
-    let canvas = evt.data.canvas;
-    let arToolkitContext = evt.data.artContext;
-    let text = evt.data.texture;
-    let gl = canvas.getContext("webgl");
-
-};
