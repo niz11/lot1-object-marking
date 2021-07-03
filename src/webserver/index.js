268f4e75b7e9e65727c6d27fb65d0651d70387f9
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2021-07-03 18:03:28 +0200

    updated index.js for nginx based server

--- src/webserver/index.js
+++ src/webserver/index.js
@@ -4,17 +4,18 @@ const path = require('path');
 const mongoose = require('mongoose');
 const models = require('./database-model/models');
 const bodyParser = require('body-parser');
+const users = require('./database-model/users');
 
 
 const app = express();
 const port = 3000;
 const useMongoDB = true;
 
-app.all('/', function(req, res, next) {
-    res.header("Cross-Origin-Embedder-Policy", "require-corp");
-    res.header("Cross-Origin-Opener-Policy", "same-origin");
-    next();
-});
+// app.all('/', function(req, res, next) {
+//     res.header("Cross-Origin-Embedder-Policy", "require-corp");
+//     res.header("Cross-Origin-Opener-Policy", "same-origin");
+//     next();
+// });
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -32,10 +33,14 @@ if (useMongoDB) {
         }
     );
 }
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static/index123.html'));
+});
 
 app.use(express.static(path.join(__dirname,'static')));
+
 app.use('/models', models);
+app.use('/users', users);
 app.set('trust proxy', 'loopback');
 app.listen(port, 'localhost', () => {
     console.log(`server is listening on ${port}`);
