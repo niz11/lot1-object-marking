da9b0b5b63ff499179e6624026e6a0b4ad102177
Sultan Numyalai <sn@go2-markets.com>
2021-07-12 01:42:32 +0430

    [FIX] request handlers for sign-in and adding userId to all services

---
+++ src/webserver/static/public/javascripts/requestHandler.js
@@ -0,0 +1,34 @@
+function handleRequest(e)
+{
+    console.log(e);
+    const request = {
+        email: document.querySelector("#email").value,
+        password: document.querySelector("#password").value
+    };
+    if(request.email && request.password)
+    {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                displayResults(xhr.responseText);
+            }
+        };
+        xhr.open("POST", "https://localhost:3000/users/login");
+        xhr.setRequestHeader("Content-type", "application/json; encoding=UTF-8");
+        xhr.send(JSON.stringify(request));
+        console.log(request);
+    }
+    return false
+}
+function displayResults(data)
+{
+    window.location.search = `userId=${data}`
+    window.location.href ="client/client_homepage.html?" + `userId=${data}`
+}
+
+function handleUrl(data)
+{
+    var params = new URLSearchParams(window.location.search);
+    var userId = params.get('userId')
+    window.location.href =`/${data}.html?` + `userId=${userId}`
+}
