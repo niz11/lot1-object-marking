01deb86631b0c857c962c1a606b95ccd18c9b582
Niz <nizan.goldstein@gmail.com>
2021-07-13 09:58:04 +0200

    Fix requestHandler, not to include userId in params if deos not exist

--- src/webserver/static/public/javascripts/requestHandler.js
+++ src/webserver/static/public/javascripts/requestHandler.js
@@ -1,60 +1,54 @@
-function register(e)
-{
-    const request = {
-        name: document.querySelector("#name").value,
-        email: document.querySelector("#email").value,
-        password: document.querySelector("#password").value
-    };
-    if(request.email && request.password && request.name)
-    {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                displayResults(xhr.responseText);
-            }
-            else
-            {
-                document.querySelector("#error").innerHTML = xhr.responseText;
-            }
-        };
-        xhr.open("POST", "https://localhost:3000/users/register");
-        xhr.setRequestHeader("Content-type", "application/json; encoding=UTF-8");
-        xhr.send(JSON.stringify(request));
-        console.log(request);
-    }
-    return false
+function register(e) {
+	const request = {
+		name: document.querySelector('#name').value,
+		email: document.querySelector('#email').value,
+		password: document.querySelector('#password').value
+	};
+	if (request.email && request.password && request.name) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				displayResults(xhr.responseText);
+			} else {
+				document.querySelector('#error').innerHTML = xhr.responseText;
+			}
+		};
+		xhr.open('POST', 'https://localhost:3000/users/register');
+		xhr.setRequestHeader('Content-type', 'application/json; encoding=UTF-8');
+		xhr.send(JSON.stringify(request));
+		console.log(request);
+	}
+	return false;
 }
 
-function handleRequest(e)
-{
-    const request = {
-        email: document.querySelector("#email").value,
-        password: document.querySelector("#password").value
-    };
-    if(request.email && request.password)
-    {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                displayResults(xhr.responseText);
-            }
-        };
-        xhr.open("POST", "https://localhost:3000/users/login");
-        xhr.setRequestHeader("Content-type", "application/json; encoding=UTF-8");
-        xhr.send(JSON.stringify(request));
-        console.log(request);
-    }
-    return false
+function handleRequest(e) {
+	const request = {
+		email: document.querySelector('#email').value,
+		password: document.querySelector('#password').value
+	};
+	if (request.email && request.password) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				displayResults(xhr.responseText);
+			}
+		};
+		xhr.open('POST', 'https://localhost:3000/users/login');
+		xhr.setRequestHeader('Content-type', 'application/json; encoding=UTF-8');
+		xhr.send(JSON.stringify(request));
+		console.log(request);
+	}
+	return false;
 }
-function displayResults(data)
-{
-    window.location.search = `userId=${data}`
-    window.location.href ="client/client_homepage.html?" + `userId=${data}`
+function displayResults(data) {
+	window.location.search = `userId=${data}`;
+	window.location.href = 'client/client_homepage.html?' + `userId=${data}`;
 }
 
-function handleUrl(data)
-{
-    var params = new URLSearchParams(window.location.search);
-    var userId = params.get('userId')
-    window.location.href =`/${data}.html?` + `userId=${userId}`
+function handleUrl(data) {
+	var params = new URLSearchParams(window.location.search);
+	var userId = params.get('userId');
+	const page = data ? `/${data}.html` : 'client/client_homepage.html';
+	const userIdParam = userId ? `?userId=${userId}` : '';
+	window.location.href = page + userIdParam;
 }
