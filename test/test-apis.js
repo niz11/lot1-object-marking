8c915d20f39a2abcaff87c6d44d5ef09d15ecfa0
Sultan Numyalai <sn@go2-markets.com>
2021-08-13 15:03:34 +0200

    [TEST] adding new mocha tests

--- test/test-apis.js
+++ test/test-apis.js
@@ -78,7 +78,7 @@ describe("POST /users/register", () => {
 
     });
 
-    it("1. It should GEt user's models", (done) => {
+    it("1. It should GET user's models", (done) => {
         const data = new TextEncoder().encode(
             JSON.stringify(
                 {
@@ -132,12 +132,61 @@ describe("GET /models", () => {
         const req = https.request(options, res => {
             console.log(`statusCode: ${res.statusCode}`)
             res.on('data', d => {
-                process.stdout.write("Response: " + d)
+                //process.stdout.write("Response: " + d)
             })
         })
         req.on('error', error => {
             console.error("Error: " + error)
         })
+        req.end()
+        done()
+
+    });
+});
+
+describe("POST /models/add-model", () => {
+
+    it("1. It should POST a new model", (done) => {
+        const data = new TextEncoder().encode(
+            JSON.stringify(
+                {
+                    modelName : "AstrunautLocal",
+                    src : "saved-models/Astronaut.glb",
+                    alt :"Astro",
+                    "hotspots[0][position]": "-0.5158334257401533m 0.8808310669112648m 00.12073262739521484m",
+                    latitude : "52.48979763883894",
+                    longitude: "13.390766864570562",
+                    "hotspots[0][normal]" :"-0.6055749170967261m -0.030052908207539756m 0.7952206250416061m",
+                    "hotspots[0][text]" : "Hand Moon2",
+                    distance :"5",
+                    rotation :"5",
+                    scaling : "5",
+                    userId :"60d6d57bdbb0861ff7cca78b",
+                }
+            )
+        )
+        const options = {
+            "rejectUnauthorized": false,
+            host: 'localhost',
+            port: 3000,
+            path: '/users/register',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
+        }
+        const req = https.request(options, res => {
+            console.log(`statusCode: ${res.statusCode}`)
+            res.on('data', d => {
+                //process.stdout.write("Response: " + d)
+            })
+        })
+        req.on('error', error => {
+            console.error("Error: " + error)
+        })
+
+        req.write(data)
         req.end()
         done()
 
